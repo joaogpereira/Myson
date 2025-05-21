2cb52587f97e624c8b42bf327fb4122cab05c344
joao.guilherme4 <bilejg@gmail.com>
2025-05-21 09:50:56 -0300

    Arrumando layout da tela wizard

--- app/src/main/java/com/example/mysonapplication/WizardActivity.java
+++ app/src/main/java/com/example/mysonapplication/WizardActivity.java
@@ -1,6 +1,9 @@
 package com.example.mysonapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,34 @@ public class WizardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wizard);
+
+
+        EditText edtNomeBebe = findViewById(R.id.edt_nome_bebe);
+        EditText edtIdadeBebe = findViewById(R.id.edt_idade_bebe);
+        ImageView imgMenino = findViewById(R.id.img_menino);
+        ImageView imgMenina = findViewById(R.id.img_menina);
+
+        // Variável para armazenar a imagem selecionada
+        final ImageView[] selectedImage = {null};
+
+        imgMenino.setOnClickListener(v ->{
+
+            if (selectedImage[0] != null) {
+                selectedImage[0].setBackgroundColor(Color.TRANSPARENT);
+            }
+            imgMenino.setBackgroundColor(Color.BLUE);
+            selectedImage[0] = imgMenino;
+        });
+
+        imgMenina.setOnClickListener(v ->{
+
+            if (selectedImage[0] != null) {
+                selectedImage[0].setBackgroundColor(Color.TRANSPARENT);
+            }
+            imgMenina.setBackgroundColor(Color.MAGENTA);
+            selectedImage[0] = imgMenina;
+        });
+
 
     }
 }
