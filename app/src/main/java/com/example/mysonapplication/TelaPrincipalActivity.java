53d5bb44af97d7e9c1fa1a0d9501969c8ab7a66d
joao-guilherme-costa <bilejg@gmail.com>
2025-05-29 22:28:51 -0300

    Criando o direcionamento para as telas de relatório

--- app/src/main/java/com/example/mysonapplication/TelaPrincipalActivity.java
+++ app/src/main/java/com/example/mysonapplication/TelaPrincipalActivity.java
@@ -1,9 +1,13 @@
 package com.example.mysonapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,6 +18,33 @@ public class TelaPrincipalActivity extends MudarTemaActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
+
+        ImageView imgViewAlimentacao = findViewById(R.id.imageViewAlimentacao);
+        ImageView imgViewSono = findViewById(R.id.imageViewSono);
+
+
+        imgViewAlimentacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        TelaPrincipalActivity.this,
+                        RelatorioAlimentacaoActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+
+        imgViewSono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        TelaPrincipalActivity.this,
+                        RelatorioSonoActivity.class
+                );
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
